22b17cd8ed02469080051d24957e1ede60135cf0
Devtron718 <devin.miranda@gmail.com>
2022-12-01 23:32:34 -0500

    revert pwa

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/ServiceWorker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
